152bd5e2c940dfa38d1a116b432281e7fa26ab5a
Angeline Polidano <angelinepolidano@gmail.com>
2021-11-18 15:02:18 +1100

    old changes

---
+++ app/controllers/parking_spaces_controller.rb
@@ -0,0 +1,80 @@
+class ParkingSpacesController < ApplicationController
+  before_action :set_parking_space, only: %i[ show edit update destroy ]
+  # skip_before_action :is_authorized, only: [:index]
+
+  # GET
+  def index
+    @parking_spaces = ParkingSpace.all
+    render :json => @parking_spaces
+  end
+
+  # GET
+  def show
+  end
+
+  # GET
+  def new
+    @parking_space = ParkingSpace.new
+  end
+
+  # GET
+  def edit
+  end
+
+  # POST
+  def create
+    @parking_space = ParkingSpace.new(parking_space_params)
+    if @parking_space.save
+      render json: {
+        status: :created,
+        parking_space: @parking_space
+      } else
+        render json: {
+          status: 500,
+          errors: @parking_space.errors.full_messages
+        }
+      end
+    # respond_to do |format|
+    #   if @parking_space.save
+    #     format.html { redirect_to @parking_space, notice: "Parking space was successfully created." }
+    #     format.json { render :show, status: :created, location: @parking_space }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @parking_space.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
+
+  # PATCH/PUT
+  def update
+    respond_to do |format|
+      if @parking_space.update(parking_space_params)
+        format.html { redirect_to @parking_space, notice: "Parking space was successfully updated." }
+        format.json { render :show, status: :ok, location: @parking_space }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @parking_space.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE
+  def destroy
+    @parking_space.destroy
+    respond_to do |format|
+      format.html { redirect_to parking_space_url, notice: "Parking space was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_parking_space
+      @parking_space = ParkingSpace.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def parking_space_params
+      params.require(:parking_space).permit(:parking_spaces_id, :user_id, :parking_space_type, :latitude, :longitude, :access, :undercover, :plug_profile_image, :vehicle_size, :description, :profile_image, :price, :parking_space, :created_at, :updated_at)
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+    skip_before_action :is_authorized, only: [:create, :login, :index]
+
+    def user_profile
+        render json: @user
+    end
+
+    def index
+        @users = User.all
+
+        render json: @users
+    end
+
+    def create
+        @user = User.create(user_params)
+        @token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+        render json: {user: @user, token: @token}, status: :created
+    end
+
+    def login
+        @user = User.find_by(email: params[:user][:email])
+
+        if @user && @user.authenticate(params[:user][:password])
+          @token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+
+          render json: {user: @user, token: @token}
+        else
+          render json: {error: "Invalid Credentials"}, status: :unauthorized
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :name)
+    end
+
+end
