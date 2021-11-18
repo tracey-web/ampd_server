57f6c2a990b41269fc8fa43f5f4b7207a04b1160
Tracey <traceylea9@gmail.com>
2021-11-18 21:05:55 +1100

    testing cors

--- config.ru
+++ config.ru
@@ -4,14 +4,14 @@ require_relative 'config/environment'
 
 run Rails.application
 
-require 'rack/cors'
-use Rack::Cors do
+# require 'rack/cors'
+# use Rack::Cors do
 
-  # allow all origins in development
-  allow do
-    origins '*'
-    resource '*', 
-        :headers => :any, 
-        :methods => [:get, :post, :delete, :put, :options]
-  end
-end
+#   # allow all origins in development
+#   allow do
+#     origins '*'
+#     resource '*', 
+#         :headers => :any, 
+#         :methods => [:get, :post, :delete, :put, :options]
+#   end
+# end
--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,12 @@ module AmpdServer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # config.middleware.insert_before 0, Rack::Cors do
-    #   allow do
-    #     origins '*'
-    #     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
-    #   end
-    # end
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
